4f04806059f74433dfa3ee55f68c5271c44d8ba6
FranckCHAMBON <franck.chambon@ac-aix-marseille.fr>
2020-06-29 15:20:12 +0200

    avec angle

--- Maths/8-Produit-scalaire/produit_scalaire.py
+++ Maths/8-Produit-scalaire/produit_scalaire.py
@@ -1,11 +1,14 @@
 from math import sqrt, cos
 
 def norme(x, y):
-	"Retourne la norme du vecteur (x, y)"
+	"""Retourne la norme du vecteur (x, y)
+	>>> norme(3, 4)
+	5.0
+	"""
 	return sqrt(x*x + y*y)
 
 # un test
-assert norme(3, 4) == 5
+assert norme(3, 4) == 5.0
 
 def produit_scalaire(x1, y1, x2, y2):
 	"""
@@ -22,3 +25,17 @@ def produit_scalaire(x1, y1, x2, y2):
 assert produit_scalaire(13, 17, 0, 1) == 17
 assert produit_scalaire(13, 17, 1, 0) == 13
 
+def angle_entre_deux_vecteurs(x1, y1, x2, y2):
+    """
+    Renvoie l'angle formé par les vecteurs (x1, y1) et (x2, y2),
+    angle en degré
+    """
+    produit_scalaire = x1*x2 + y1*y2
+    norme1 = norme(x1, y1)
+    norme2 = norme(x2, y2)
+    cos_angle = produit_scalaire / (norme1 * norme2)
+    angle_rad = acos(cos_angle) # en radians
+    return angle_rad * 180 / pi
+
+assert round(angle_entre_deux_vecteurs(4, -3, 1, 5)) == 116
+   
